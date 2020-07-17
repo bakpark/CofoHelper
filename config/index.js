caa41ae1002337b5b438e37494529409b70bb9fb
bakpark <bakparkbj@gmail.com>
2020-07-18 05:54:34 +0900

    test host change

--- config/index.js
+++ config/index.js
@@ -14,7 +14,7 @@ module.exports = {
 
     // Various Dev Server settings
     // host: 'localhost', // can be overwritten by process.env.HOST
-    host: '15.165.170.174', // can be overwritten by process.env.HOST
+    host: '172.31.43.112', // can be overwritten by process.env.HOST
     port: 8081, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,

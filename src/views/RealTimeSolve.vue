cd224abb60dd111edec30d7b06be4fb5df8fe6e7
bakpark <bakparkbj@gmail.com>
2020-07-23 03:49:44 +0900

    버튼 UI 변경

--- src/views/RealTimeSolve.vue
+++ src/views/RealTimeSolve.vue
@@ -12,18 +12,35 @@
   width: 90%;
   margin-top: 2em;
   display: flex;
+  margin-bottom: 2em;
 }
 .group-button {
   margin: 0.3em;
   height: 3em;
   width: 8em;
 }
+button.hoverable_btn_group {
+  color: rgb(248, 255, 255);
+  background-color: #337882;
+  height: 2em;
+  width: 5em;
+  font-size: 2em;
+  font-weight: 600;
+  border-radius: 10px;
+  border: none;
+  margin: 3px;
+}
+button.hoverable_btn_group:hover {
+  border: 2px solid #337882;
+  background-color:rgb(248, 255, 255);
+  color: #337882;
+}
 </style>
 <template>
   <div class="realtime-solve">
     <div class="body-wrapper">
       <div class="button-wrapper">
-        <button class="group-button" v-for="group in $store.state.groups" :key="group.groupName" @click="selectGroup(group)"> {{ group.groupName }}</button>
+        <button class="hoverable_btn_group" v-for="group in $store.state.groups" :key="group.groupName" @click="selectGroup(group)"> {{ group.groupName }}</button>
       </div>
     </div>
     <GroupSubmits :group="selectedGroup"></GroupSubmits>
